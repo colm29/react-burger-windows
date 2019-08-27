f8b85d8d172b722708a531f3ac5040240c87b345
colm29 <colm29@gmail.com>
2019-08-27 15:42:22 +0100

    Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
+import classes from './Layout.module.css'
 
 const layout = (props) => (
     <Fragment>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Fragment>
